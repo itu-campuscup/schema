1e88142210c106b25ca89939befe0da33a32f0b7
lucasfth <online@lucashanson.dk>
2026-08-30 09:46:23 +0200

    fix(stats): import generated http action

--- statsApi.ts
+++ statsApi.ts
@@ -1,4 +1,4 @@
-import { httpAction } from "convex/server";
+import { httpAction } from "./_generated/server";
 import { api } from "./_generated/api";
 
 const STATS_API_KEY = process.env.STATS_API_KEY;
